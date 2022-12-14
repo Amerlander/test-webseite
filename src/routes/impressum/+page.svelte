68f1834f8616669bb6300a49ae46d9a0a07c200f
André Ferdinand <andre.ferdinand.roddahn@gmail.com>
2022-12-14 17:35:46 +0100

    add content

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,4 +1,16 @@
-
-<h1>Impressum</h1>
-
-<p>vdfgdfgfgfd</p>
+Impressum
+Angaben gemäß § 5 TMG:
+Tausendweg e.V.
+Roddahner Dorfstraße 9
+16845 Neustadt (Dosse)
+Kontakt:
+Telefon: +4933973809711
+E:Mail: 
+Registereintrag:
+Eintragung im Vereinsregister des Amtsgerichts Berlin-Charlottenburg.
+Registernummer: VR 16497 B
+Für die Webseite verantwortlicher (alleinvertretungsberechtigter) Vorstand:
+André Ferdinand-Kroll
+Webdesign:
+Mio Sachtleber
+Quelle: http://www.e-recht24.de
