02368fd6b31807fa72c1e2b3bcaacc3c913ed11c
André Ferdinand <andre.ferdinand.roddahn@gmail.com>
2022-12-14 17:54:30 +0100

    add page content

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,16 +1,16 @@
-Impressum
-Angaben gemäß § 5 TMG:
-Tausendweg e.V.
-Roddahner Dorfstraße 9
-16845 Neustadt (Dosse)
-Kontakt:
-Telefon: +4933973809711
-E:Mail: 
-Registereintrag:
-Eintragung im Vereinsregister des Amtsgerichts Berlin-Charlottenburg.
-Registernummer: VR 16497 B
-Für die Webseite verantwortlicher (alleinvertretungsberechtigter) Vorstand:
-André Ferdinand-Kroll
-Webdesign:
-Mio Sachtleber
-Quelle: http://www.e-recht24.de
+<h1>Impressum</h1>
+<p>Angaben gem&auml;&szlig; &sect; 5 TMG:</p>
+<p>Tausendweg e.V.</p>
+<p>Roddahner Dorfstra&szlig;e 9</p>
+<p>16845 Neustadt (Dosse)</p>
+<h2><strong>Kontakt:</strong></h2>
+<p><strong><strong>Telefon:&nbsp;</strong></strong>+4933973809711</p>
+<p><strong><strong>E:Mail:&nbsp;</strong></strong></p>
+<h2><strong>Registereintrag:</strong></h2>
+<p><span size="3">Eintragung im Vereinsregister des Amtsgerichts Berlin-Charlottenburg.</span></p>
+<p><span size="3">Registernummer: VR 16497 B</span></p>
+<h2><strong><strong>F&uuml;r die Webseite verantwortlicher (alleinv</strong></strong><strong><strong>ertret</strong></strong><strong><strong>ungsberechtigter)</strong></strong><strong><strong>&nbsp;Vorstand:</strong></strong></h2>
+<p><strong>Andr&eacute; Ferdinand-Kroll</strong></p>
+<h2><strong>Webdesign:</strong></h2>
+<p><strong>Mio Sachtleber</strong></p>
+<p>Quelle: <em>http://www.e-recht24.de</em></p>
